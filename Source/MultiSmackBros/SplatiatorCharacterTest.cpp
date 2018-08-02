793600e6a291c7acca6ccb41e96774c16f5833f4
GD48_JuanValencia <juanjvalenciagd@gmail.com>
2018-08-01 19:56:10 -0700

    Tried implementing attacks.

--- Source/MultiSmackBros/SplatiatorCharacterTest.cpp
+++ Source/MultiSmackBros/SplatiatorCharacterTest.cpp
@@ -1,14 +1,27 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SplatiatorCharacterTest.h"
-
-void ASplatiatorCharacterTest::BeginPlay()
-{
-	this->AutoPossessPlayer = EAutoReceiveInput::Player1;
-}
-
+#include "TimerManager.h"
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+	
 void ASplatiatorCharacterTest::NeutralAttack()
 {
+	UBoxComponent *hurtBox;
+
+	if ( GetWorldTimerManager().GetTimerRemaining(NeutralAttackTime) <= 0.0f)
+	{
+		GetWorldTimerManager().SetTimer(NeutralAttackTime, this, &ACharacterBase::CloseDash, 0.5f, false);
+		IsNeutralAttack = true;
+		hurtBox = CreateDefaultSubobject<UBoxComponent>("Box", false);
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, "Attack");
+	}
+		
 	
+}
+
+void ASplatiatorCharacterTest::ExitAttack()
+{
+	IsNeutralAttack = false;
 }
 
