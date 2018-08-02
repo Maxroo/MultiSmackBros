28110b5eb24f6852e872125e048149cfc3e1f7eb
Hao-Tse <maxwuhz@gmail.com>
2018-08-02 03:26:37 -0700

    Camera on the c++ update

--- Source/MultiSmackBros/GameCamera.cpp
+++ Source/MultiSmackBros/GameCamera.cpp
@@ -1,21 +1,32 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GameCamera.h"
-
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Actor.h"
 
 // Sets default values
 AGameCamera::AGameCamera()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	OurCameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraSpringArm"));
+	OurCameraSpringArm->SetupAttachment(RootComponent);
+	OurCameraSpringArm->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(0.0f, 0.0f, 0.0f));
+	OurCameraSpringArm->TargetArmLength = 400.f;
+	OurCameraSpringArm->bEnableCameraLag = true;
+	OurCameraSpringArm->CameraLagSpeed = 3.0f;
+	OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("GameCamera"));
+	OurCamera->SetupAttachment(OurCameraSpringArm, USpringArmComponent::SocketName);
+	
+	
 }
 
 // Called when the game starts or when spawned
 void AGameCamera::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	//test view blend 
 }
 
 // Called every frame
--- Source/MultiSmackBros/GameCamera.h
+++ Source/MultiSmackBros/GameCamera.h
@@ -4,25 +4,37 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include	 "Camera/CameraComponent.h"
+#include "GameFramework/Pawn.h"
+#include "GameFramework/SpringArmComponent.h"
 #include "GameCamera.generated.h"
 
 UCLASS()
 class MULTISMACKBROS_API AGameCamera : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+
+public:
 	// Sets default values for this actor's properties
 	AGameCamera();
+	AActor* myactor;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	UPROPERTY(EditAnywhere)
+		USpringArmComponent* OurCameraSpringArm;
+	UCameraComponent* OurCamera;
+
+	UPROPERTY(Category = "AutoPlayerActivation", EditAnywhere)
+		TEnumAsByte<EAutoReceiveInput::Type> AutoActivateForPlayer;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	
-	
+
+
 };
